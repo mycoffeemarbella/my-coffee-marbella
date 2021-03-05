434835dcf683da0f3acec54fb569f5f9b0876261
mycoffeemarbella <mycoffeebranding@gmail.com>
2021-03-05 09:27:56 +0000

    added uber justeats, changed colour in almuerzos

--- src/@lekoarts/gatsby-theme-jodie/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-jodie/components/footer.tsx
@@ -3,6 +3,8 @@ import { jsx, Link, useThemeUI } from "theme-ui"
 import { readableColor } from "polished"
 import useSiteMetadata from "../hooks/use-site-metadata"
 import Glovo from '../../../../static/glovo.png'
+import Uber from '../../../../static/ubereats.png'
+import JustEat from '../../../../static/justeat.png'
 
 const Footer = ({ bg }: { bg: string }) => {
   const { siteTitle } = useSiteMetadata()
@@ -34,7 +36,9 @@ const Footer = ({ bg }: { bg: string }) => {
       }}
     >
       <div>    
-      <Link href="https://glovoapp.com/en/mba/store/my-coffe-marbella-marbella/"> <img src={Glovo} height="30px" alt="glovo"/> </Link>
+      <Link href="https://glovoapp.com/es/en/marbella/my-coffee-marbella-mba/"> <img src={Glovo} height="30px" alt="glovo"/> </Link>
+      <Link href="https://www.ubereats.com/es/malaga/food-delivery/my-coffee-marbella/02QmE2jASxq9a_fZveS7eg/d3a8c8ec-3719-43b4-b454-23c20557f73f"> <img src={Uber} height="30px" alt="uber"/> </Link>
+      <Link href="https://www.just-eat.es/restaurants-my-coffe-marbella-marbella/menu?utm_source=google&utm_medium=organic&utm_campaign=orderaction"> <img src={JustEat} height="30px" alt="justeat"/> </Link>
       </div>
       <div>
         &copy; {new Date().getFullYear()} by {siteTitle}.
